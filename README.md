6ebe78ef65cb594b75feb71dceaa3adefc4b9c34
mipastgt <mp@jugs.org>
2023-04-01 10:26:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+PolySpiral Desktop
+
+A simple Compose Desktop Application to play with jDeploy for distribution.
+
+- `./gradlew run` - run application
+- `./gradlew package` - package native distribution into `build/compose/binaries`
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import de.mpmediasoft.polyspiral.gui.PolySpiralApp
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        PolySpiralApp()
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
